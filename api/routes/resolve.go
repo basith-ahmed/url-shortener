5fe0e6f4d20b39394cec573c76853e9268bc6539
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-06-13 21:27:23 +0530

    resolver

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,5 +1,29 @@
 package routes
 
-import ()
+import (
+	"github.com/basith-ahmed/url-shortener/database"
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+)
 
-func 
+func ResolveUrl(ctx *fiber.Ctx) error {
+	url := ctx.Params("url")
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	// get the url
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{"error": "Short URL not found!"})
+	} else if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": "Internal error."})
+	}
+
+	// increment the view value
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+
+	_ = rInr.Incr(database.Ctx, "counter")
+
+	return ctx.Redirect(value, 301)
+}
