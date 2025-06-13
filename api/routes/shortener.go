a20ebab319fd0203204e90897b431bf6d5551911
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-06-13 17:29:37 +0530

    shortener

--- api/routes/shortener.go
+++ api/routes/shortener.go
@@ -1,6 +1,10 @@
 package routes
 
-import "time"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 type request struct {
 	URL         string        `json:"url"`
@@ -14,4 +18,26 @@ type response struct {
 	Expiry          time.Duration `json:"expiry"`
 	XRateRemaining  int           `json:"rate_limit"`
 	XRateLimitReset time.Duration `json:"rate_limit_reset"`
+}
+
+func ShortenUrl(ctx *fiber.Ctx) error {
+	body := new(request)
+
+	if err := ctx.BodyParser(&body); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Cannot parse JSON"})
+	}
+
+	// - rate limiting -
+
+	// - validate -
+	if !govalidator.IsURL(body.URL) {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error": "Invalid IP"})
+	}
+
+	if !helpers.RemoveDomainError(body.URL) {
+		return ctx.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Domain error"})
+	}
+
+	// - SSL -
+	body.URL = helpers.EnforceHTTP(body.URL)
 }
