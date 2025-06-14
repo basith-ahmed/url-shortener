0e236c38f367b519fac6d84ba25a2f015532582a
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-06-14 23:28:51 +0530

    rate limit

--- api/routes/shortener.go
+++ api/routes/shortener.go
@@ -1,9 +1,13 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/basith-ahmed/url-shortener/database"
 	"github.com/basith-ahmed/url-shortener/helpers"
+	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -29,6 +33,19 @@ func ShortenUrl(ctx *fiber.Ctx) error {
 	}
 
 	// - rate limiting -
+	r := database.CreateClient(1) //redis
+	defer r.Close()
+	val, err := r.Get(database.Ctx, ctx.IP()).Result()
+	if err == redis.Nil {
+		_ = r.Set(database.Ctx, ctx.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+		val, _ = r.Get(database.Ctx, ctx.IP()).Result()
+		valInt, _ := strconv.Atoi(val)
+		if valInt <= 0 {
+			limit, _ := r.TTL(database.Ctx, ctx.IP()).Result()
+			return ctx.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{"error": "Rate limit exceeded", "rate_limit_reset": limit / time.Nanosecond / time.Minute})
+		}
+	}
 
 	// - validate -
 	if !govalidator.IsURL(body.URL) {
@@ -41,4 +58,7 @@ func ShortenUrl(ctx *fiber.Ctx) error {
 
 	// - SSL -
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	r.Decr(database.Ctx, ctx.IP())
+
 }
