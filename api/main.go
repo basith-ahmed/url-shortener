a30fe459e6d2a50cbdc04c3721b8f3c99e3009a0
Basith Ahmed N A <basithahmed.dev@gmail.com>
2025-06-15 09:50:46 +0530

    vendor packages

--- api/main.go
+++ api/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/basith-ahmed/url-shortener/routes"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
